395bfe0e5ba932943a38d6da331f9bbf5aa48c53
nemanja731 <nemanja.jankovic731@gmail.com>
2023-04-25 00:18:38 +0200

    Update

--- code/source/Main.cpp
+++ code/source/Main.cpp
@@ -11,13 +11,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     string configurationFileName = argv[1], fileName = argv[2];
-    string logFileName = fileName.substr(0, fileName.length() - 4) + ".log";
-
-    // open log file
-    Writer::getInstance().open(logFileName);
 
     // read configuration file
-    Configuration::getInstance().readConfiguration(configurationFileName);
+    Configuration::getInstance().readConfiguration(configurationFileName, fileName);
 
     // creates compiler
     Compiler *compiler = new Compiler;
@@ -27,9 +23,6 @@ int main(int argc, char *argv[])
 
     // execute imf file with Machine
     Machine::getInstance().execute(imfFileName);
-
-    // close log file
-    Writer::getInstance().close();
 
     return 0;
 }
